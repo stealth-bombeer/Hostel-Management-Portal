700a95d64f3203d0f94301e3c11fd632e06c56e7
DEVelooper29 <devbbhuva@gmail.com>
2023-02-15 12:16:12 +0530

    Admin filter

--- client/src/pages/AdminHome.js
+++ client/src/pages/AdminHome.js
@@ -4,7 +4,7 @@ import { useAuthContext } from "../hooks/useAuthContext"
 const AdminHome = () => {
   const [dat, setDat] = useState([]);
   const { user } = useAuthContext();
-  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [selectedBlockNo, setSelectedBlockNo] = useState('');
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -23,11 +23,23 @@ const AdminHome = () => {
   }, []);
 
   const handleSelectRoom = (students) => {
-    setSelectedRoom(students);
+    setSelectedBlockNo(students);
   }
+
+  const filteredDat = selectedBlockNo
+    ? dat.filter((item) => item.BlockNo === selectedBlockNo)
+    : dat;
 
   return (
     <div className="announcement-box">
+      <select value={selectedBlockNo} onChange={(e) => setSelectedBlockNo(e.target.value)}>
+        <option value="">Select a block number</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+        <option value="D">D</option>
+        <option value="E">E</option>
+      </select>
       <table>
         <thead>
           <tr>
@@ -39,27 +51,25 @@ const AdminHome = () => {
           </tr>
         </thead>
         <tbody>
-          {dat && dat.map((date) => (
+          {filteredDat.map((date) => (
             <tr key={date._id}>
               <td>{date.BlockNo}</td>
               <td>{date.FloorNo}</td>
               <td>{date.RoomNo}</td>
               <td>{date.Students.length}</td>
               <td>
-              
-              <ul>
-              {date.Students.map((student) => (
-              <li key={student._id}>{student.Name}</li>
-              ))}
-              </ul>
-              
+                <ul>
+                  {date.Students.map((student) => (
+                    <li key={student._id}>{student.Name}</li>
+                  ))}
+                </ul>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      
     </div>
   );
 }
+
 export default AdminHome;
